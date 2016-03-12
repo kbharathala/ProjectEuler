076f14133c0e07856739e590634956cf781f0ebc
kbharathala <kbharathala@gmail.com>
2016-03-11 17:24:06 -0800

    Solved 44

--- Question 44_2.py
+++ Question 44_2.py
@@ -4,23 +4,18 @@ start = time.time()
 def pentagonal(n):
     return(int(((n*(3*n-1))/2)))
 
-def checker(num,list):
-    for n in range(0, len(list)):
-        if num > list[n]:
-            if num == list[n-1]:
-                return True
-            else:
-                return False
-size = 1000
+size = 3000
 pent = []
-for each in range(1,size):
-    pent.append(pentagonal(each))
-for n in range(1,size,1):
+
+for i in range(1,size):
+    pent.append(pentagonal(i))
+
+for n in range(1,size):
+    print(n)
     for num in range(0,size-n-1):
-        if checker((pent[num]+pent[num+n]),pent):
-            print(pent[num],pent[num+n])
-            if checker((pent[num+n]-pent[num]),pent):
-                print("YEHSEYHSYEHSYEHSYEHSYESHEY" + str((pent[num+n]-pent[num])))
+        if (pent[num]+pent[num+n] in pent):
+            #print(pent[num],pent[num+n])
+            if (pent[num+n]-pent[num] in pent):
+                print(pent[num+n]-pent[num])
 
 print (time.time() - start)
-        
