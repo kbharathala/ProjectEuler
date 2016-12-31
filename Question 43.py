b443e369399f447ded5dba65e3f749b2220cedaa
kbharathala <kbharathala@gmail.com>
2016-12-30 22:13:02 -0800

    solved 43

--- Question 43.py
+++ Question 43.py
@@ -1,14 +1,27 @@
-def is_pandigital(num,n):
-    test_case = []
-    list = []
-    string = str(num)
-    for all in range(1,n+1):
-        test_case.append(all)
-    for each in string:
-        list.append(int(each))
-    list.sort()
-    if list == test_case:
-        return True
-    return False
+import itertools
+import time
 
+start = time.time()
 
+def pandigitals():
+    digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    p = list(itertools.permutations(digits))
+    return [''.join(x) for x in p if x[0] != '0']
+
+def div_list(n):
+    return [x for x in range(1000) if x % n == 0]
+
+def div_lists():
+    primes = [2, 3, 5, 7, 11, 13, 17]
+    return [div_list(x) for x in primes]
+
+d = div_lists()
+l = pandigitals()
+
+for loop in range(6, -1, -1):
+    l = [x for x in l if int(x[loop+1:loop+4]) in d[loop]]
+print(sum([int(x) for x in l]))
+
+print(time.time() - start)
+
+#16695334890
