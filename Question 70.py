28de7e30334e7a52c9415218ed3e3a120f8b07ea
kbharathala <kbharathala@gmail.com>
2017-01-01 14:23:43 -0800

    tried a couple questions

--- Question 70.py
+++ Question 70.py
@@ -1,28 +1,28 @@
-def is_prime(num):
-    if num == 2:
-        return True
-    for i in range(2,int(pow(num,.5)+1),1):
-        if num % i == 0:
-            return False
-    return True
+from collections import Counter
+import time
 
-def rel_prime(a,b): # a < b
-    if is_prime(a) and b%a != 0:
-        return True
-    if is_prime(b):
-        return True
-    for num in range(2,a+1):
-        if a % num == 0 and b % num == 0:
-            return False
-    return True
+start = time.time()
+
+def gcd(x, y):
+    return x if y == 0 else gcd(y, x % y)
 
 def totient(n):
-    ans = 0
-    for num in range(2,n):
-        if num % 100 == 0:
-            print(num)
-        if rel_prime(num,n):
-            ans += 1
-    return ans
+    return len([x for x in range(n) if gcd(n, x) == 1])
 
-print(totient(87109))
+# def score(n):
+#     return float(n)/totient(n)
+
+def is_permutation(a, b):
+    if len(str(a)) != len(str(b)):
+        return False
+    return Counter(list(str(b))) == Counter(list(str(a)))
+
+l = []
+for x in xrange(2, 2000):
+    t = totient(x)
+    if is_permutation(x, t):
+        l.append((float(x) / t, t, x))
+
+print(l)
+
+print(time.time() - start)
