1f474a64d4c28bbc04300c07518d37547bfcd970
kbharathala <kbharathala@gmail.com>
2017-01-01 11:41:44 -0800

    moved files

--- Problem 40.py
+++
@@ -1,6 +0,0 @@
-string = "0"
-a = 1
-while len(string) < 1000001:
-    string = string + str(a)
-    a = a + 1
-print(string)
--- euler_toitent.py
+++
@@ -1,8 +0,0 @@
-def totient(num):
-    list = []
-    for i in range(1,num,1):
-        if num % i != 0:
-            list.append(i)
-    return list
-
-print(totient(5))
