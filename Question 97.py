7f034a8f9ae6f9081b7801b791a9391a9dfc5b6c
kbharathala <kbharathala@gmail.com>
2017-01-02 23:15:12 -0800

    Solved 97

--- Question 97.py
+++ Question 97.py
@@ -1,1 +1,11 @@
-int = str(28433 * pow(2,7830457) + 1)[-10:]
+import time
+
+start = time.time()
+ 
+n = 2
+for i in xrange(7830456):
+    n = (2 * n) % 10000000000
+ 
+print((n * 28433 + 1) % 10000000000)
+
+print(time.time() - start)
